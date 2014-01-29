388e76b6dbd1dd5836e133cfd470f6b5d61dd453
Lukáš Čellár <cellar.lukas@gmail.com>
2014-01-29 23:02:09 +0100

    css/js update

--- www_root/js/error-monitoring.js
+++ www_root/js/error-monitoring.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-   $('body').on('click', 'a.btn-solve.grid-ajax', function(e) {
-       e.preventDefault();
-       var href = $(this).attr('href');
-       $(this).closest('tr').fadeOut(function() {
-           $.get(href);
-       });
-   });
+	$('body').on('click', 'a.btn-solve.grid-ajax', function(e) {
+		e.preventDefault();
+		var href = $(this).attr('href');
+		$(this).closest('tr').fadeOut(function() {
+			$.get(href);
+		});
+	});
+	
+	$('body').on('click', 'a.btn-load-exceptions', function() {
+		$(this).removeClass('btn-danger');
+		$(this).addClass('btn-warning');
+		$(this).html('Updating...');
+	});
+	
+	$("a.ajax").live("click", function(event) {
+		event.preventDefault();
+		$.get(this.href);
+	});
+	
+	$("form.ajax").live("submit", function() {
+		$(this).ajaxSubmit();
+		return false;
+	});
+	
+	$("form.ajax :submit").live("click", function() {
+		$(this).ajaxSubmit();
+		return false;
+	});
 });
